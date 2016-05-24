1c8809ff111242c6f6985ceba5507a78e9f0b3fa
Hans Schouten <J.Schouten-1@student.tudelft.nl>
2016-05-24 11:03:56 +0200

    Test project structure updated

---
+++ src/AircraftTrajectories.NUnit.Tests/Models/Space3D/MetricToGeographicTest.cs
@@ -0,0 +1,20 @@
+﻿using AircraftTrajectories.Models.Space3D;
+using NUnit.Framework;
+using System;
+
+namespace AircraftTrajectories.NUnit.Tests.Models.Space3D
+{
+    [TestFixture]
+    class MetricToGeographicTest
+    {
+        [Test]
+        public void convertToLatLong()
+        {
+            var converter = new MetricToGeographic();
+            var metricPoint = converter.ConvertToLongLat(122202, 487250);
+
+            Assert.AreEqual("52.372", Math.Round(metricPoint.Y, 3).ToString());
+            Assert.AreEqual("4.906", Math.Round(metricPoint.X, 3).ToString());
+        }
+    }
+}
