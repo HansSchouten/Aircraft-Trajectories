f0e702210f7aca69471f0efebd610fec7689b07a
Ekula <e.y.kula@gmail.com>
2016-05-25 10:00:24 +0200

    Added conversion tests

--- src/AircraftTrajectories.NUnit.Tests/Models/Space3D/CoordinateConversionTest.cs
+++ src/AircraftTrajectories.NUnit.Tests/Models/Space3D/CoordinateConversionTest.cs
@@ -9,17 +9,56 @@ namespace AircraftTrajectories.NUnit.Tests.Models.Space3D
     class CoordinateConversionTest
     {
         [Test]
-        public void ConvertCoordinatesTest()
+        public void ConvertMetricToGeographic()
         {
             
             Point3D input = new Point3D(122202, 487250, 122202, CoordinateUnit.metric);
             CoordinateConversion converter = new CoordinateConversion();
             Point3D result = converter.ConvertCoordinates(input, CoordinateUnit.geographic);
-
             Assert.AreEqual("52.372", Math.Round(result.Y, 3).ToString());
             Assert.AreEqual("4.906", Math.Round(result.X, 3).ToString());
             Assert.AreEqual("122202", Math.Round(result.Z, 3).ToString());
 
         }
+
+        [Test]
+        public void ConvertImperial()
+        {
+
+            Point3D input = new Point3D(122202, 487250, 122202, CoordinateUnit.imperial);
+            CoordinateConversion converter = new CoordinateConversion();
+            Point3D result = converter.ConvertCoordinates(input, CoordinateUnit.metric);
+            Assert.AreEqual("148513.8", Math.Round(result.Y, 3).ToString());
+            Assert.AreEqual("37247.17", Math.Round(result.X, 3).ToString());
+            Assert.AreEqual("37247.17", Math.Round(result.Z, 3).ToString());
+
+        }
+
+        [Test]
+        public void ConvertGeographic()
+        {
+
+            Point3D input = new Point3D(122202, 487250, 122202, CoordinateUnit.geographic);
+            CoordinateConversion converter = new CoordinateConversion();
+            Point3D result = converter.ConvertCoordinates(input, CoordinateUnit.metric);
+            Assert.AreEqual("1118889.975", Math.Round(result.Y, 3).ToString());
+            Assert.AreEqual("13603464413.919", Math.Round(result.X, 3).ToString());
+            Assert.AreEqual("122202", Math.Round(result.Z, 3).ToString());
+
+        }
+
+        [Test]
+        public void ConvertMetricToImperial()
+        {
+
+            Point3D input = new Point3D(122202, 487250, 122202, CoordinateUnit.metric);
+            CoordinateConversion converter = new CoordinateConversion();
+            Point3D result = converter.ConvertCoordinates(input, CoordinateUnit.imperial);
+            Assert.AreEqual("1598589.239", Math.Round(result.Y, 3).ToString());
+            Assert.AreEqual("400925.197", Math.Round(result.X, 3).ToString());
+            Assert.AreEqual("400925.197", Math.Round(result.Z, 3).ToString());
+
+        }
     }
+
 }
--- src/AircraftTrajectories.NUnit.Tests/Models/TemporalGrid/TemporalGridTest.cs
+++ src/AircraftTrajectories.NUnit.Tests/Models/TemporalGrid/TemporalGridTest.cs
@@ -4,23 +4,26 @@ using NUnit.Framework;
 using AircraftTrajectories.Models.IntegratedNoiseModel;
 
 namespace AircraftTrajectories.NUnit.Tests.TemporalGrid
-
 {
+    using AircraftTrajectories.Models.IntegratedNoiseModel;
+
     [TestFixture]
     public class TemporalGridTest
     {
-/**
+        IntegratedNoiseModel noiseModel;
+
         [Test]
         public void TemporalGrid()
-        {
+        { /**
             var reader = new TrajectoryFileReader(CoordinateUnit.metric);
             var trajectory = reader.createTrajectoryFromFile(Globals.testdataDirectory + "test_track.dat");
 
             Aircraft aircraft = new Aircraft("GP7270", "wing");
-            var noiseModel = IntegratedNoiseModel.IntegratedNoiseModel(trajectory, aircraft);
+            noiseModel = IntegratedNoiseModel.IntegratedNoiseModel(trajectory, aircraft);
 
             var temporalGrid = noiseModel.TemporalGrid;
-        } */
+    */
+        } 
 
     }
 }
