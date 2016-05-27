7d0ec6b903830553315ddc0271aeadb568d9f51c
Hans Schouten <J.Schouten-1@student.tudelft.nl>
2016-05-27 17:14:03 +0200

    PopulationDataTest added

---
+++ src/AircraftTrajectories.NUnit.Tests/Models/Population/PopulationDataTest.cs
@@ -0,0 +1,19 @@
+﻿using AircraftTrajectories.Models.Population;
+using NUnit.Framework;
+
+namespace AircraftTrajectories.NUnit.Tests.Models.Population
+{
+
+    [TestFixture]
+    public class PopulationDataTest
+    {
+        [Test]
+        public void GetPopulationDataTest()
+        {
+            var populationData = new PopulationData(Globals.testdataDirectory + "population.dat");
+            populationData.Chance = 1;
+            var data = populationData.getPopulationData();
+            Assert.AreEqual(2, data.Count);
+        }
+    }
+}
