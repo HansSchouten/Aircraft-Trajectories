b59f99de18213e73eb061d0efcd9d3d552d67074
Ekula <e.y.kula@gmail.com>
2016-06-14 01:23:26 +0200

    Added Optimization tests

---
+++ src/AircraftTrajectories.NUnit.Tests/Models/Optimisation/TrajectoryChromosomeTest.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using GeneticSharp.Domain.Chromosomes;
+using GeneticSharp.Domain.Fitnesses;
+using System.Collections.Generic;
+using AircraftTrajectories.Models.Optimisation;
+
+
+namespace AircraftTrajectories.Models.Optimisation
+{
+    [TestFixture]
+
+    class TrajectoryChromosomeTest
+    {
+        [Test]
+        public void NumberOfSegmentsTest()
+        {
+            TrajectoryChromosome chromosome = new TrajectoryChromosome(3, 4);
+
+            Assert.AreEqual(4, chromosome.NumberOfSegments);
+        }
+
+        [Test]
+        public void GenerateChromsomeTest()
+        {
+            TrajectoryChromosome chromosome = new TrajectoryChromosome(3, 4);
+            chromosome.CreateNew();
+            Assert.AreEqual(3, chromosome.Length);
+            Assert.AreEqual(4, chromosome.NumberOfSegments);
+        }
+
+        [Test]
+        public void GenerateGeneTest()
+        {
+            TrajectoryChromosome chromosome = new TrajectoryChromosome(3, 4);
+            chromosome.GenerateGene(2);
+
+            Assert.IsNotNull(chromosome.GetGene(2));
+        }
+
+        [Test]
+        public void ChromosomeLengthTest()
+        {
+            TrajectoryChromosome chromosome = new TrajectoryChromosome(3, 4);
+            Assert.AreEqual(17, Optimisation.TrajectoryChromosome.ChromosomeLength(5));
+        }
+    }
+}
---
+++ src/AircraftTrajectories.NUnit.Tests/Models/Optimisation/TrajectoryFitnessTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using GeneticSharp.Domain.Chromosomes;
+using GeneticSharp.Domain.Fitnesses;
+using System.Collections.Generic;
+using AircraftTrajectories.Models.Optimisation;
+
+
+namespace AircraftTrajectories.NUnit.Tests.Models.Optimisation
+{
+    [TestFixture]
+
+    class TrajectoryFitnessTest
+    {
+        [Test]
+        public void FitnessTest()
+        {
+            TrajectoryFitness fitness = new TrajectoryFitness();
+            TrajectoryChromosome chromosome = new TrajectoryChromosome(10, 3);
+            double fuel = fitness.Evaluate(chromosome);
+
+            Assert.AreEqual(24537, fuel, 10000);
+        }
+    }
+}
