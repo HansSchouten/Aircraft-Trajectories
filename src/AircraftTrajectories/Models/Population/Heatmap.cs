20e2dbfe245cf270d1fedfb1a62a57df3b5977fb
Ekula <e.y.kula@gmail.com>
2016-06-22 23:36:45 +0200

    Heatmaps added to visualization

---
+++ src/AircraftTrajectories/Models/Population/Heatmap.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AircraftTrajectories.Models.Population
+{
+    using Space3D;
+    using System;
+    using System.Drawing;
+    using System.Globalization;
+    using System.IO;
+    using TemporalGrid;
+
+    public class Heatmap
+    {
+        protected string _file;
+        public int[][] _populationData;
+
+        public Heatmap(string file)
+        {
+            _file = file;
+        }
+
+        public void getPopulationArray()
+        {
+            string rawData = File.ReadAllText(_file);
+            _populationData = rawData
+                .Split('\n')
+                .Select(q =>
+                    q.Split(new[] { "\t" }, StringSplitOptions.RemoveEmptyEntries)
+                     .Select(x => {
+                         return (int)decimal.Parse(x, NumberStyles.Float);
+                     })
+                     .ToArray()
+                )
+                .ToArray();
+
+        }
+
+
+        public void inputGHeat()
+        {
+            RDToGeographic converter = new RDToGeographic();
+
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(@"points.txt"))
+
+                foreach (int[] row in _populationData)
+                {
+                    PointF point = converter.convertToLatLong(row[0], row[1]);
+
+                    file.WriteLine(row[2] + "," + point.Y + "," + point.X);
+                }
+
+        }
+
+
+
+    }
+}
